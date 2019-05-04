e6ea5c7a0617b8a38673b207e5adcf92257cb3c7
John Kane <Ctznkane525@yahoo.com>
2019-05-04 18:51:36 -0400

    Created
    Initial Creation

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=autumnsolarsystembot-Web-Deploy.pubxml -p:Password=balldpikkeXw4TgN2kQRlrMHcq9bA0djph7f71pz2CxAmQEk8KZncqfaSMdE
+
